847970462281b3400d41ceab64e66670950df852
zaferCW <zafer@clarusway.com>
2023-08-09 23:49:54 +0300

    proje tamamlandi java egitimi bitti

--- src/ju.java
+++ src/ju.java
@@ -9,13 +9,32 @@ public class ju {
     safak hanimlar
 
 
-    team 1 sunucular mustafa bey serap hanim sena hanim zerrrin hanim emina K hanim derya hanim
+    team 1 sunucular jira da github da var
+    mustafa bey
+    serap hanim
+    sena hanim
+    zerrrin hanim
+    emina K hanim
+    derya hanim
+    zeynep hanim
 
-    team 2 rumeysa hanim taha bey
+    team 2 github var
+    rumeysa hanim
+    taha bey
+    safak hanim
+    emine hanim
+    meryem hanim
 
-    team 3 furkan bey , taha bey
+    team 3
+    furkan bey ,
+     taha bey
+     ezgisu hanim
+     iclal hanim
+     aytekin bey
 
-    team 4 ahmet bey omer bey
+    team 4
+    ahmet bey
+    omer bey
 
 
      */
